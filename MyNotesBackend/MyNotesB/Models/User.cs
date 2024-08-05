970aacdf8754ed058b6c3a6465373aee355f128c
vesnazubac <105123324+vesnazubac@users.noreply.github.com>
2024-08-05 16:03:12 +0200

    Added database configuration

---
+++ MyNotesBackend/MyNotesB/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyNotesB.Models
+{
+    public class User
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set;}
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string ProfilePicture { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+    }
+}
