f0b9e00286522036b29c38bcecb36a4170c5dfaf
vesnazubac <105123324+vesnazubac@users.noreply.github.com>
2024-08-13 15:50:36 +0200

    Added signalR and reminders function

---
+++ MyNotesBackend/MyNotes.Application/Features/Notifications/NotificationHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNotes.Application.Features.Notifications
+{
+        public class NotificationHub:Hub
+    {
+        public async Task SendReminder(string message)
+        {
+            await Clients.All.SendAsync("ReceiveReminder", message);
+        }
+    }
+}
